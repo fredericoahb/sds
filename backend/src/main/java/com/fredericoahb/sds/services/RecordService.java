bc6beb5dd43a18a694554847c83723e4fbdee219
Frederico Balieiro <fredericoahb@gmail.com>
2020-12-13 20:02:24 -0300

    GET records

--- backend/src/main/java/com/fredericoahb/sds/services/RecordService.java
+++ backend/src/main/java/com/fredericoahb/sds/services/RecordService.java
@@ -3,6 +3,8 @@ package com.fredericoahb.sds.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,25 +17,30 @@ import com.fredericoahb.sds.repositories.RecordRepository;
 
 @Service
 public class RecordService {
-
+	
 	@Autowired
 	private RecordRepository repository;
-
+	
 	@Autowired
 	private GameRepository gameRepository;
-
+	
 	@Transactional
 	public RecordDTO insert(RecordInsertDTO dto ) {
 		Record entity = new Record();
 		entity.setName(dto.getName());
 		entity.setAge(dto.getAge());
 		entity.setMoment(Instant.now());
-
+		
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
-
+		
 		entity = repository.save(entity);
 		return new RecordDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoment(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 
 }
