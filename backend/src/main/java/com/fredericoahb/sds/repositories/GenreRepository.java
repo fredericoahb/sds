b0d8bbca9c6a6b90cbe3d780eb531b4d5a712a4a
Frederico Balieiro <fredericoahb@gmail.com>
2020-12-13 19:49:14 -0300

    DTO, Service

--- backend/src/main/java/com/fredericoahb/sds/repositories/GenreRepository.java
+++ backend/src/main/java/com/fredericoahb/sds/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.fredericoahb.sds.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fredericoahb.sds.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
---
+++ backend/src/main/java/com/fredericoahb/sds/services/GameService.java
@@ -0,0 +1,26 @@
+package com.fredericoahb.sds.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fredericoahb.sds.dto.GameDTO;
+import com.fredericoahb.sds.entities.Game;
+import com.fredericoahb.sds.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll() {
+		List<Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+
+}	
