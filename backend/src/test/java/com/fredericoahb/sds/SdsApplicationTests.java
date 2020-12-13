4a941b6878dd3a6d745ce132c883089ba9bc8d90
Frederico Balieiro <fredericoahb@gmail.com>
2020-12-13 15:56:51 -0300

    Project Created

---
+++ backend/src/test/java/com/fredericoahb/sds/SdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredericoahb.sds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
