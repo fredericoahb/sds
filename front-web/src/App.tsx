356fb1c153e74d20be1bd537acaca3d64f177b1b
Frederico Balieiro <fredericoahb@gmail.com>
2020-12-13 21:30:31 -0300

    Creating Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/index';
+import Home from './pages/Home'
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
